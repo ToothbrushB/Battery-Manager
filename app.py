fb041c32be149efb5fe870ea78cd6f7f16837f4e
ToothbrushB <49379586+ToothbrushB@users.noreply.github.com>
2025-12-03 13:36:15 -0600

    Settings & env

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-
+import dotenv
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -10,15 +10,9 @@ from helpers import apology
 # Configure application
 app = Flask(__name__)
 
-# Custom filter
-
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///database.db")
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 
 @app.after_request
@@ -29,16 +23,62 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
+@app.context_processor
+def inject_status():
+    return {"status": {"name": "Operational", "positive": True, "icon": "check-circle"}}
 
-@app.route("/") 
+@app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.route("/list_view", methods=["GET"])
 def list_view():
     return render_template("list_view.html")
 
+
+@app.route("/settings", methods=["GET", "POST"])
+def settings():
+    config = [
+        {
+            "legend": "Snipe-IT Settings",
+            "settings": [
+                {
+                    "id": "snipe-url",
+                    "name": "snipe-url",
+                    "label": "Snipe-IT URL",
+                    "type": "text",
+                    "value": "https://assets.yourorghere.org/api/v1/",
+                },
+                {
+                    "id": "snipe-api-key",
+                    "name": "snipe-api-key",
+                    "label": "Snipe-IT API Key",
+                    "type": "text",
+                    "value": "your-api-key-here",
+                }
+            ]
+        },
+        {
+            "legend": "TBA Settings",
+            "settings": [
+                {
+                    "id": "tba-url",
+                    "name": "tba-url",
+                    "label": "TBA URL",
+                    "type": "text",
+                    "value": "https://www.thebluealliance.com/api/v3",
+                },
+                {
+                    "id": "tba-api-key",
+                    "name": "tba-api-key",
+                    "label": "TBA API Key",
+                    "type": "text",
+                    "value": "your-api-key-here",
+                }
+            ]
+        }
+    ]
+    return render_template("settings.html", config=config)
 
 
 @app.route("/grid_view", methods=["GET"])
